4f72c91f4ea0f6c90ada9b39e7e75e4a9461be4a
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-20 15:04:20 +0300

    feat: user management logic

---
+++ pages/Auth/forgot-password.vue
@@ -0,0 +1,72 @@
+<template>
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div role="alert" v-if="error" class="rounded border-s-4 border-red-500 bg-red-50 p-4">
+                        <strong class="block font-medium text-red-800"> Something went wrong </strong>
+
+                        <p class="mt-2 text-sm text-red-700">
+                            {{ error }}
+                        </p>
+                    </div>
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Forgot password
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                email</label>
+                            <input type="email" name="email" id="email" v-model="form.email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="name@company.com" required="">
+                        </div>
+
+                        <button type="button" @click="handleSubmit"
+                            class="w-full text-black bg-sky-500/25 hover:bg-sky-500/50 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ForgotPassword',
+    data() {
+        return {
+            error: "",
+            form: {
+                email: '',
+            }
+        }
+    },
+    methods: {
+        async handleSubmit() {
+            try {
+                const response = await $fetch('http://127.0.0.1:8000/users/forgot-password/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: {
+                        email: this.form.email,
+                    }
+                });
+                console.log(response);
+                if (response.code !== "200") {
+                    return this.error = response.message
+                }
+                return await navigateTo(`/auth/reset-password/${this.form.email}`)
+
+            } catch (error) {
+                this.error = "Connection error"
+                console.error('Error:', error);
+            }
+
+        }
+    }
+}
+</script>
---
+++ pages/Auth/register.vue
@@ -0,0 +1,131 @@
+<template>
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div role="alert" v-if="error" class="rounded border-s-4 border-red-500 bg-red-50 p-4">
+                        <strong class="block font-medium text-red-800"> Something went wrong </strong>
+
+                        <p class="mt-2 text-sm text-red-700">
+                            {{ error }}
+                        </p>
+                    </div>
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Create an account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                username</label>
+                            <input type="text" name="username" id="username" v-model="form.username"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="username" required="">
+                        </div>
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                email</label>
+                            <input type="email" name="email" id="email" v-model="form.email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="name@company.com" required="">
+                        </div>
+                        <div>
+                            <label for="password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                v-model="form.password1"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required="">
+                        </div>
+                        <div>
+                            <label for="confirm-password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                                password</label>
+                            <input type="password" name="confirm-password" id="confirm-password"
+                                placeholder="••••••••" v-model="form.password2"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required="">
+                        </div>
+                        <div>
+                            <label for="role"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                            <select name="role" id="role" v-model="form.role"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="attendee" selected>Event attendee</option>
+                                <option value="creator" selected>Event creator</option>
+                            </select>
+                        </div>
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input id="terms" aria-describedby="terms" type="checkbox"
+                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    required="">
+                            </div>
+                            <div class="ml-3 text-sm">
+                                <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a
+                                        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                        href="#">Terms and Conditions</a></label>
+                            </div>
+                        </div>
+                        <button type="button" @click="handleSubmit"
+                            class="w-full text-black bg-sky-500/25 hover:bg-sky-500/50 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
+                            an account</button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Already have an account? <a href="/auth/login"
+                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Register',
+    data() {
+        return {
+            error: "",
+            form: {
+                username: '',
+                email: '',
+                firstName: '',
+                lastName: '',
+                password1: '',
+                password2: '',
+                role: ''
+            }
+        }
+    },
+    methods: {
+        async handleSubmit() {
+            try {
+                const response = await $fetch('http://127.0.0.1:8000/users/register/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: {
+                        username: this.form.username,
+                        email: this.form.email,
+                        password1: this.form.password1,
+                        password2: this.form.password2,
+                        user_role: this.form.role
+                    }
+                });
+
+                if (response.code !== "201") {
+                    return this.error = response.message
+                }
+                return await navigateTo(`/auth/confirm-email/${this.form.email}`)
+
+            } catch (error) {
+                this.error = "Connection error"
+                console.error('Error:', error);
+            }
+
+        }
+    }
+}
+</script>

471293e1d787e16c13194e29086f7f29cccdfd05
Geekigen <kigencaleb50@gmail.com>
2024-02-24 18:06:27 +0300

    chages on index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,76 @@
 <template>
-    <div>
-        Index is here
+   <div>
+  <section class="bg-gray-200 ">
+    <div
+      class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+    >
+      <div class="mx-auto max-w-lg text-center">
+        <h2 class="text-3xl font-bold sm:text-4xl text-blue-600">What kind of exchange are you looking for</h2>
+  
+      </div>
+  
+      <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        
+<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <a href="#">
+      <img class="p-8 rounded-t-lg" src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1421&q=80" alt="product image" />
+  </a>
+  <div class="px-5 pb-5">
+      
+     <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">Apple Watch Series Starlight Sport</h5>
+      <p class="text-white mb-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores nam inventore </p>
+  
+      <div class="flex items-center justify-between">
+          
+          <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">view host</a>
+      </div>
+  </div>
+</div>
+
+<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <a href="#">
+      <img class="p-8 rounded-t-lg" src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1421&q=80" alt="product image" />
+  </a>
+  <div class="px-5 pb-5">
+      
+     <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">Apple Watch Series Starlight Sport</h5>
+      <p class="text-white mb-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores nam inventore </p>
+  
+      <div class="flex items-center justify-between">
+          
+          <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">view host</a>
+      </div>
+  </div>
+</div>
+<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <a href="#">
+      <img class="p-8 rounded-t-lg" src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1421&q=80" alt="product image" />
+  </a>
+  <div class="px-5 pb-5">
+      
+     <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">Apple Watch Series Starlight Sport</h5>
+      <p class="text-white mb-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores nam inventore </p>
+  
+      <div class="flex items-center justify-between">
+          
+          <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">view host</a>
+      </div>
+  </div>
+</div>
+
+        
+      </div>
+  
+      <div class="mt-12 text-center">
+        <a
+          href="#"
+          class="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+        View full host list
+        </a>
+      </div>
     </div>
+  </section>
+</div>
+
 </template>

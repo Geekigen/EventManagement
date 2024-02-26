6db8fc40573eab4c48077ab8eafc02eb4f1156b5
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-26 17:59:44 +0300

    feat: add store

---
+++ store.js
@@ -0,0 +1,52 @@
+export const authStore = defineStore({
+    id: "authStore",
+    state: () => {
+        return{
+            user: {},
+            permissions:[],
+            token:"",
+            loggedIn: false
+        }
+    },
+    actions:{
+        setUser(user){
+            this.user = user
+        },
+        setToken(token){
+            this.token = token
+        },
+        setPermissions(permissions){
+            this.permissions = permissions
+        },
+        login(){
+            this.loggedIn = true
+        },
+        logout(){
+            this.loggedIn = false
+        }
+    },
+
+    getters:{
+        getUser:(state)=>{
+            return state.user
+        },
+        getToken:(state)=>{
+            return state.token
+        },
+        getPermission:(state)=>{
+            return (name) => state.permissions.find((permission)=> permission == name)
+        },
+        isLoggedIn:(state)=>{
+            return state.loggedIn
+        }
+
+    },
+
+    persist:true
+
+})
+
+/// changes are detected
+if(import.meta.hot){
+    import.meta.hot.accept(acceptHMRUpdate(authStore, import.meta.hot))
+}

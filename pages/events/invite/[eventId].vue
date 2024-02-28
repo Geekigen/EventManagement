1ff09cf9a8ed2154ec9e4024b989a8a466720d7a
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-28 12:16:54 +0300

    refractor: rename button

--- pages/events/invite/[eventId].vue
+++ pages/events/invite/[eventId].vue
@@ -20,7 +20,7 @@
                                 placeholder="email" required="">
                         </div>
                         <button type="button" @click="handleSubmit"
-                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send invite</button>
                        
                     </form>
                 </div>
